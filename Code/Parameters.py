3fa39fafe4e3389d77d0a960b228a5c707c45423
Shura <maoall5@yandex.ru>
2022-07-28 15:39:55 +0300

    Some useful files

---
+++ Code/Parameters.py
@@ -0,0 +1,5 @@
+# This file contains all parameters for modeling and rings
+
+Thickness = 0
+Number  = 5
+Radius  = 1
---
+++ Code/Ring_Class.py
@@ -0,0 +1,15 @@
+from Parameters import Number, Radius, Thickness
+
+class Ring():
+    N = Number   # Number of rings in system
+    r = Radius   # Radius of the rings
+    h = Thickness   # Thickness of the rings
+
+    def __int__(self, x, y, z, pos):
+        self.x = x      # x position of the ring
+        self.y = y      # y position of the ring
+        self.z = z      # z position of the ring
+        self.pos = pos  # orientation of the ring - "xy" or "xz" or "zy"
+
+    def __repr__(self):
+        return "x = " + str(self.x) + " y = " + str(self.y) + " z = " + str(self.z) + " orientation: " + self.pos
