9ae745f70fbbe0dfe4de9a35534244527113d120
Shura <maoall5@yandex.ru>
2022-12-21 05:00:17 +0300

    Final comments

---
+++ Code/Comparing_matrix.py
@@ -0,0 +1,52 @@
+# Comparing geometric matrix of metamaterial MRI lenz
+
+from Parameters import a, a1, Radius, Rings
+
+cnt_sign = 0        # Counter of rings with sign errors
+cnt_abs = 0         # Counter of rings with large absolute error
+
+# Matrix produced from verified code
+# To compare is necessary to multiply at normalized length
+with open("DATA/matrica.txt", "r") as res:
+    RES = res.read()
+    Strings = RES.split("\n")[:len(RES.split("\n"))-1]
+    M1 = [[float(k) * Radius for k in x.split(" ")] for x in Strings]
+#M1 = [[float(M[i][k]) for k in range(len(M[i]))] for i in range(len(M)-1)]
+
+
+# Matrix from python - code
+
+with open("DATA/Data.txt", "r") as res:
+    RES = res.read()
+    Strings = RES.split("\n")[:len(RES.split("\n")) - 1]
+    M2 = [[float(k)*(a1/a) ** 1 for k in x.split(" ")] for x in Strings]
+
+# Ratios between which elements are equal
+Ratio_lowest = 7
+Ratio_highest = 12
+
+print("Comparing in absolute ratio \n")
+for i in range(len(M1)):
+    for j in range(len(M1[i])):
+        # Avoid zero division on diagonal
+        if i != j:
+            id = M2[i][j]/M1[i][j]
+            if not (Ratio_lowest < id < Ratio_highest):
+                cnt_abs += 1
+                print(f"Indexes and ratio :{i, j, id}")
+                print(f"Values: {M2[i][j], M1[i][j]}")
+                print(f'Parameters for integrate: dx = {Rings[j].x-Rings[i].x}, dy = {Rings[j].y-Rings[i].y}, dz = {Rings[j].z-Rings[i].z} \n')
+
+print("Comparing signs of values \n")
+for i in range(len(M1)):
+    for j in range(len(M1[i])):
+        # Avoid zero division on diagonal
+        if i != j:
+            id = M2[i][j]/M1[i][j]
+            if not id < 0:
+                cnt_sign += 1
+                print(f"Indexes and ratio :{i, j, id}")
+                print(f"Values: {M2[i][j], M1[i][j]}")
+                print(f'Parameters for integrate: dx = {Rings[j].x-Rings[i].x}, dy = {Rings[j].y-Rings[i].y}, dz = {Rings[j].z-Rings[i].z} \n')
+
+
