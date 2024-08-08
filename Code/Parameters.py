49c5a3f1442aefbc6003d3153a9a5def8ebde96e
broshura <maoall5@yandex.com>
2024-08-08 13:18:34 +0300

    modified:   Code/Parameters.py

--- Code/Parameters.py
+++ Code/Parameters.py
@@ -1,19 +1,18 @@
 # This file contains all parameters for modeling and geometry of rings
-import os
-import sys
 
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from Ring_Class import Ring
+from Impedance_matrix import M_diag
 from numpy import pi
 import numpy as np
-
 
 # Parameters for system used in modeling
 
 L = 13.459 * 10 ** -9                   # Self-inductance
-C = 470 * 10 ** -12                     # Capacitance
-R = 0.0465                              # Resistance
-omega_0 = float(62.46 * 1e6 * 2*np.pi)  # Resounance frequency for effective magnetic permeability
+C = 470 * 10 ** -10                     # Capacitance
+R = 0.002                               # Resistance
+omega_0 = 1 / np.sqrt(L * C)            # Resonance frequency
+Omega =  np.linspace(omega_0*0.9, omega_0*1.1, 1000) # Frequency range
+H_0z = 1                                # Amplitude of magnetic field
+mu_0 = 4 * pi * 10 ** -7                # Magnetic constant
 
 Dz = 15 * 10 ** -3                      # Length of cell
 Dy = Dz
@@ -25,9 +24,6 @@ W = 0.7 * 0.15 * Dz * 0                 # Width of strip
 Sigma = -0.06                           # Lattice constant
 
 Orientations = 'zyx'                    # Orientations of rings
-# Adding responding ring to identify resonance frequency
-
-name = "MRI-self"
 
 Params = {
     'L': L,                     # Self-inductance
@@ -42,7 +38,24 @@ Params = {
     'shift_y': 0,               # Shifting of next layer along y axes
     'shift_z': 0,               # Shifting of next layer along z axes
     'Orientations': Orientations,
-    'Self-frequence': omega_0,  # Resonance frequency for effective magnetic permeability
     'Sigma': Sigma,
+    'H_0z': H_0z,
+    'Omega': Omega,
+    'mu_0': mu_0,
+    'omega_0': omega_0,
+    'M_0': M_diag,
+    'Solver_type': 'fast',
+    'Packing': 'Rectangle',
+    'P_0z': pi * Radius ** 2 /H_0z/Dz/Dy/Dx,
+    'N' : {
+        'z':{'nz': 11, 'ny': 10, 'nx': 10},
+        'y':{'nz': 10, 'ny': 11, 'nx': 10},
+        'x':{'nz': 10, 'ny': 10, 'nx': 11}
+    },
+    'Numbers': {
+        'z': 1100,
+        'y': 1100,
+        'x': 1100
+    }
 }
 
